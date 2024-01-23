2013a4eb12cd3b8ceef6ffcbb281d58b7309f40c
kelipeng <kelipeng@shoplazza.com>
2024-01-23 10:46:54 +0800

    feat:模版标签

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,15 +1,19 @@
 <template>
-  <h3>文本插值</h3>
-
-  <p>打招呼: {{ name }}</p>
+  <h3>模版标签</h3>
+  <p>打招呼: {{ msg }}</p>
+  <p>AGE: {{ age + 1 }}</p>
+  <p>{{ flag?"YES":"NO" }}</p>
+  <p>打招呼反转{{ msg.split("").reverse().join("") }}</p>
 </template>
 
 <script>
 export default {
   data() {
     return{
-      name: "你好呀👋"
+      msg: "你好呀👋",
+      age: 21,
+      flag:true,
     }
-  }
+  },
 }
 </script>
