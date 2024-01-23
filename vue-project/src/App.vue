1e43eb0e78acd8f5ac3d6d6c8598205bd76dbfc0
kelipeng <kelipeng@shoplazza.com>
2024-01-23 15:01:33 +0800

    feat:事件处理

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,15 +3,19 @@ import HelloWorld from './components/HelloWorld.vue'
 import AttrBind from './components/AttrBind.vue'
 import IfShow from './components/IfShow.vue'
 import ListShow from './components/ListShow.vue'
+import EventDeal from './components/EventDeal.vue'
 </script>
 
 <template>
-  <HelloWorld />
+<!--  <HelloWorld />-->
+<!--  <p>&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;</p>-->
+<!--  <AttrBind />-->
+<!--  <p>&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;</p>-->
+<!--  <IfShow />-->
+<!--  <p>&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;</p>-->
+<!--  <ListShow />-->
+<!--  <p>&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;&#45;</p>-->
+  <EventDeal />
   <p>------------------</p>
-  <AttrBind />
-  <p>------------------</p>
-  <IfShow />
-  <p>------------------</p>
-  <ListShow />
 </template>
 
---
+++ vue-project/src/components/EventDeal.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>事件处理</h3>
+  <button @click="num++">按钮1</button>
+  <button @click="num++">按钮1</button>
+  <button @click="addNum">按钮2---{{num}}</button>
+  <p>count={{num}}</p>
+</template>
+
+<script>
+export default {
+  //属性
+  data(){
+    return {
+      num:0,
+    }
+  },
+  //方法
+  methods:{
+    addNum()
+    {
+      this.num++
+    }
+  }
+}
+</script>
