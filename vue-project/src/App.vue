b0646427b02eb5dd3e7034e9a1173535cfbbf0a7
kelipeng <kelipeng@shoplazza.com>
2024-01-23 11:38:44 +0800

    feat:列表渲染

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,6 +2,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 import AttrBind from './components/AttrBind.vue'
 import IfShow from './components/IfShow.vue'
+import ListShow from './components/ListShow.vue'
 </script>
 
 <template>
@@ -10,5 +11,7 @@ import IfShow from './components/IfShow.vue'
   <AttrBind />
   <p>------------------</p>
   <IfShow />
+  <p>------------------</p>
+  <ListShow />
 </template>
 
---
+++ vue-project/src/components/ListShow.vue
@@ -0,0 +1,44 @@
+<template>
+  <h3>列表渲染</h3>
+  <p v-for="val of arrTest">{{val}}</p>
+  <ul v-for="meta of list">
+    <li :id="meta.id">{{meta.content}}----{{meta.linkStr}}</li>
+  </ul>
+
+  <p v-for="(val,index) of arrTest">{{index}}---{{val}}</p>
+
+  <div v-for="(value,key,index) of objVal">{{index}}--{{key}}---{{value}}</div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        arrTest: ["我","你","他"],
+        list:[
+          {
+            id:123,
+            content:"dsdsvebrebfdvfdbdfbd",
+            linkStr:"xxxq1",
+          },
+          {
+            id:124,
+            content:"fdvfrbtrntntny",
+            linkStr:"xxxq2",
+          },
+          {
+            id:125,
+            content:"ddfbbrtbtnryddddd",
+            linkStr:"xxxq3",
+          }
+        ],
+        objVal:{
+          name:"xxx",
+          age:12,
+          sex:"女"
+        }
+      }
+    }
+  }
+</script>
