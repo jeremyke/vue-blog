14fc04b933ff6b89db6e3428e714c9b724196e86
kelipeng <kelipeng@shoplazza.com>
2024-01-24 15:49:25 +0800

    feat:组件组成

---
+++ vue-components/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- 3.显示组件 -->
+  <MyComponent  />
+
+  <my-component  />
+</template>
+
+<script>
+//1. 引入组件
+import  MyComponent from "./components/MyComponent.vue"
+//2. 注入组件
+export default {
+  components:{
+    MyComponent
+  }
+}
+</script>
