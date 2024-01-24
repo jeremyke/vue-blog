44ba5e41bc669307f1386b222642ccf80d423d98
kelipeng <kelipeng@shoplazza.com>
2024-01-24 15:48:55 +0800

    feat:组件组成

---
+++ vue-components/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">组件基础组成</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!--scoped:让当前样式只在当前组件中生效-->
+<style scoped>
+.container {
+  color: red;
+  font-size: 30px;
+}
+</style>
