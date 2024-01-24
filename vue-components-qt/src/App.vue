ef75a22079fc9418fabf88409f74af1dbdf76666
kelipeng <kelipeng@shoplazza.com>
2024-01-24 18:29:57 +0800

    feat:组件嵌套

---
+++ vue-components-qt/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header />
+  <Main />
+  <Aside />
+</template>
+
+<script>
+import Header from "./pages/Header.vue"
+import Main from "./pages/Main.vue"
+import Aside from "./pages/Aside.vue"
+export default {
+  components:{
+    Header,
+    Main,
+    Aside
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-components-qt/src/pages/Aside.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="aside">
+    <h3>Aside</h3>
+  </div>
+</template>
+
+<style scoped>
+.aside{
+  float: right;
+  width: 30%;
+  height: 600px;
+  border: 5px solid #999;
+  box-sizing: border-box;
+}
+</style>
