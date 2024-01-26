efa03e59502e9cfcbb5df5ba52c649661aa34cd2
kelipeng <kelipeng@shoplazza.com>
2024-01-26 14:50:46 +0800

    feat:组件依赖注入

---
+++ vue-di-ioc/src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3>Child</h3>
+  <p>{{title}}</p>
+  <p>{{showMsg}}</p>
+  <p>{{content}}</p>
+  <p>{{globalData}}</p>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props:{
+    title:{
+      type:String,
+      default() {
+        return "空";
+      }
+    }
+  },
+  inject:["showMsg","content","globalData"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-di-ioc/src/components/Parent.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>Parent</h3>
+  <Child :title="$props.title"/>
+</template>
+
+<script>
+import Child from "@/components/Child.vue";
+
+export default {
+  name: "Parent",
+  components: {Child},
+  props:{
+    title:{
+      type:String
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
