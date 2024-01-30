d23d56e2454b0502ff3a24b14725d34b11dccaa4
kelipeng <kelipeng@shoplazza.com>
2024-01-30 17:11:33 +0800

    feat:element plus

---
+++ element-start/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="25%"><Aside/></el-aside>
+      <el-container>
+        <el-header><Header/></el-header>
+        <el-main><Main/></el-main>
+        <el-footer><Footer/></el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+import {defineComponent} from "vue";
+import Header from "@/components/Header.vue";
+import Aside from "@/components/Aside.vue";
+import Main from "@/components/Main.vue";
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  components: {Footer, Main, Aside, Header}
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ element-start/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="copyright">Copyright © 2019-2021 十三. All rights reserved.</p>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+.copyright{
+  text-align: center;
+}
+</style>
